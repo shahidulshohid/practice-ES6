c2a1ca0e96551fac4cf7ab5881d3e559381fbfb2
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-28 16:31:36 +0600

    all practice is done

---
+++ module-32-Es6/passBy.js
@@ -0,0 +1,25 @@
+
+
+// //primitive type are pass by value
+// let num1 = 5;
+// let num2 = 7;
+// function multiply(a, b){
+//     a = 27;
+//     const result = a * b;
+//     return result;
+// }
+// console.log(num1)
+// multiply(num1, num2);
+// console.log(num1)
+
+// //object and array are pass by reference
+// const student1 = {name:'jalil', partner:'borsha'}
+// const student2 = {name:'raj', partner:'anika'}
+
+// function makeMovie(couple1, couple2){
+//     couple1.name = 'ononto';
+//     couple2.name = 'mim';
+// }
+// console.log(student1, student2)
+// makeMovie(student1, student2)
+// console.log(student1, student2)
