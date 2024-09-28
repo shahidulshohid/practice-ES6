497c69464056fe239be9dfdbc80b215257b789d6
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-28 11:27:16 +0600

    clouser practice is done

---
+++ module-32-Es6/clouser.js
@@ -0,0 +1,26 @@
+function stopWatch(){
+    let counter = 0;
+    return function(){
+        counter ++;
+        return counter;
+    }
+    
+}
+const watch1 = stopWatch()
+console.log(watch1())
+console.log(watch1())
+console.log(watch1())
+const watch2 = stopWatch()
+console.log(watch2())
+console.log(watch2())
+console.log(watch2())
+
+
+console.log(watch1())
+console.log(watch1())
+console.log(watch1())
+
+
+console.log(watch2())
+console.log(watch2())
+console.log(watch2())
