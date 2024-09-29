4138ddc98eb1d34a5eea2bfe3f0741a8eadbdad9
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-29 16:42:07 +0600

    array spread practice done

---
+++ conceptual-session/ES6-conceptual-session-one/arraySpread.js
@@ -0,0 +1,20 @@
+// const arr = [1, 2, 3];
+// const arr2 = [...arr];
+// arr.push(44)
+// arr.push(66)
+
+// console.log(arr)
+// console.log(arr2)
+
+
+// let arr3 = [];
+// for(const item of arr){
+//     arr3.push(item)
+// }
+// console.log(arr3)
+
+const names = ['abul kasem', 'kasem mia', 'mokbul hossen']
+
+const [z, ...rest] = names
+console.log(z)
+console.log(rest)
---
+++ conceptual-session/ES6-conceptual-session-one/logical-operator.js
@@ -0,0 +1,10 @@
+const money =true;
+console.log(money && 'ami college jabo')
+
+const bus = 'bus ase';
+const rickshaw = 'rickshaw ase'
+console.log(bus || rickshaw)
+
+
+const statement = money ? 'i will go to college': 'i will not go to college';
+console.log(statement)
